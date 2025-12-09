90c874a4557858cdf75a3ce3f3f2fd93073b08e4
Nexilon <s2510629014@fhooe.at>
2025-12-09 12:45:10 +0100

    Added more Icons (30/47)

---
+++ src/characterIcons.ts
@@ -0,0 +1,65 @@
+import YeShunguangIcon from "./images/Ye_Shunguang_Icon.png";
+import ZhaoIcon from "./images/Zhao_Icon.png";
+import BanyueIcon from "./images/Banyue_Icon.png";
+import DialynIcon from "./images/Dialyn_Icon.png";
+import YidhariIcon from "./images/Yidhari_Icon.png";
+import LuciaIcon from "./images/Lucia_Icon.png";
+import ManatoIcon from "./images/Manato_Icon.png"
+import OrphieIcon from "./images/Orphie_Icon.png";
+import SeedIcon from "./images/Seed_Icon.png";
+import AliceIcon from "./images/Alice_Icon.png";
+import YuzuhaIcon from "./images/Yuzuha_Icon.png";
+import JuFufuIcon from "./images/Ju_Fufu_Icon.png";
+import YixuanIcon from "./images/Yixuan_Icon.png";
+import PanYinhuIcon from "./images/Pan_Yinhu_Icon.png";
+import HugoIcon from "./images/Hugo_Icon.png";
+import VivianIcon from "./images/Vivian_Icon.png";
+import TriggerIcon from "./images/Trigger_Icon.png";
+import Solider0AnbyIcon from "./images/Soldier_0_Anby_Icon.png";
+import PulchraIcon from "./images/Pulchra_Icon.png";
+import EvelynIcon from "./images/Evelyn_Icon.png";
+import AstraIcon from "./images/Astra_Icon.png";
+import HarumasaIcon from "./images/Harumasa_Icon.png";
+import MiyabiIcon from "./images/Miyabi_Icon.png";
+import LighterIcon from "./images/Lighter_Icon.png";
+import YanagiIcon from "./images/Yanagi_Icon.png";
+import BurniceIcon from "./images/Burnice_Icon.png";
+import CaesarIcon from "./images/Caesar_Icon.png";
+import JaneIcon from "./images/Jane_Icon.png";
+import SethIcon from "./images/Seth_Icon.png";
+import QingyiIcon from "./images/Qingyi_Icon.png";
+
+// Map character IDs to their icon image
+export const characterIcons: Record<number, string> = {
+    1: YeShunguangIcon,
+    2: ZhaoIcon,
+    3: BanyueIcon,
+    4: DialynIcon,
+    5: YidhariIcon,
+    6: LuciaIcon,
+    7: ManatoIcon,
+    8: OrphieIcon,
+    9: SeedIcon,
+    10: AliceIcon,
+    11: YuzuhaIcon,
+    12: JuFufuIcon,
+    13: YixuanIcon,
+    14: PanYinhuIcon,
+    15: HugoIcon,
+    16: VivianIcon,
+    17: TriggerIcon,
+    18: Solider0AnbyIcon,
+    19: PulchraIcon,
+    20: EvelynIcon,
+    21: AstraIcon,
+    22: HarumasaIcon,
+    23: MiyabiIcon,
+    24: LighterIcon,
+    25: YanagiIcon,
+    26: BurniceIcon,
+    27: CaesarIcon,
+    28: JaneIcon,
+    29: SethIcon,
+    30: QingyiIcon,
+    // X: newIcon
+};
